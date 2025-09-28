b4d3ecd583d2d628e7ecd57c36d81703ad95b162
whoistahito <taha.amirhs22@gmail.com>
2025-09-28 15:32:31 +0200

    Adjust endpoint and content in infer request

--- scripts/infer_request.py
+++ scripts/infer_request.py
@@ -2,17 +2,15 @@ import json
 
 import requests
 
-API_URL = "http://127.0.0.1:8000/inference"
+API_URL = "http://127.0.0.1:8000/extract"
 
 # Read the entire input_markdown_linkedin.txt file
 with open("../input_markdown_linkedin.txt", "r", encoding="utf-8") as f:
     markdown_content = f.read()
 
 payload = {
-    "model": "glm4-9b",
-    "input": {
-        "markdown": markdown_content
-    },
+    "modelId": "glm4-9b",
+    "inputText": markdown_content
 }
 
 headers = {
